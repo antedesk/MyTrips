573b47d857d0ff4f418de89880dfe0494a834f2b
Antedesk <antedesk@gmail.com>
2018-08-23 01:06:57 +0200

    basic implementation for AddActivityActivity

---
+++ app/src/main/java/it/antedesk/mytrips/AddActivityActivity.java
@@ -0,0 +1,17 @@
+package it.antedesk.mytrips;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class AddActivityActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_activity);
+    }
+
+    public void onSaveActivityClick(View view) {
+    }
+}
